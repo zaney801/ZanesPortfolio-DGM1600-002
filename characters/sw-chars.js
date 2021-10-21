366ea9683ca2b0f156ac7b4d7c91351cfd066e3f
Zane Young <utahzane@gmail.com>
2021-10-21 12:44:43 -0600

    filtering and last number

--- characters/sw-chars.js
+++ characters/sw-chars.js
@@ -1,20 +1,40 @@
-import { people } from "../data/people.js";
+import { people } from '../data/people.js'
 
-const main = document.querySelector("#main");
+const main = document.querySelector('#main')
 
-console.log(people.length);
+const maleCharacters = people.filter(person => person.gender === 'male')
+console.log(maleCharacters.length)
+const femaleCharacters = people.filter(person => person.gender === 'female')
+console.log(femaleCharacters.length)
 
-people.forEach((element, index) => {
-  const personFig = document.createElement("figure");
-  const personImg = document.createElement("img");
-  personImg.src = `https://starwars-visualguide.com/assets/img/characters/${
-    index + 1
-  }.jpg`;
-  const personCaption = document.createElement("figcaption");
-  personCaption.textContent = "Person name goes here";
+const otherCharacters = people.filter(person => {
+  if(person.gender === 'n/a' || person.gender === 'hermaphrodite') {
+    return person
+  }
+})
 
-  personFig.appendChild(personImg);
-  personFig.appendChild(personCaption);
+console.log(otherCharacters)
 
-  main.appendChild(personFig);
-});
+femaleCharacters.forEach((element) => {
+  const personFig = document.createElement('figure')
+  const personImg = document.createElement('img')
+  let charNum = getLastNumber(element.url)
+  personImg.src = `https://starwars-visualguide.com/assets/img/characters/${charNum}.jpg`
+  const personCaption = document.createElement('figcaption')
+  personCaption.textContent = element.name
+
+  personFig.appendChild(personImg)
+  personFig.appendChild(personCaption)
+
+  main.appendChild(personFig)
+  
+})
+
+function getLastNumber(url) {
+  let end = url.lastIndexOf('/')
+  let start = end - 2
+  if (url.charAt(start) === '/') {
+    start++
+  }
+  return url.slice(start, end)
+}
--- codejournal.js
+++ codejournal.js
@@ -1,441 +1,128 @@
 /* Variables - containers that store values 
    Multi-line comment here */
 
-var name; // a declared variable, but not initialized and it's in the global scope (BAD)
-
-let foo; // a declared variable that can be changed
-
-//const bar; // a declared variable that cannot be changed - short for 'constant'
-
-const ANSWER = 42; // const is declared and initialized with the value 42
-
-// Strings
-
-let string1 = "Hello World!"; // preferred way
-
-let string2 = new String("Hello World!"); // constructor
-
-// Number
-
-let myNum = 29038424;
-
-let myNum2 = 345.89;
-
-"1" == 1; // this statement is true because of type coercion and loose equality checking
-"1" === 1; // false because this is strict equality checking
-
-// Boolean
-
-let myBool = false;
-
-// Array
-
-let myArray = []; // this is an array
-
-//              0     1      2        3      4
-let myArray2 = [42, "Bob", myBool, ANSWER, true];
-
-let secondElement = myArray2[1]; // the second position is at index #1
-
-myArray2.push("Thor"); // added an element to the end of myArray2
-
-myArray2.unshift("Hello World!");
-
-let mylongString =
-  "32408usfjalieriweur938u425ksdjfowiur84uwrlwshdjfo8wuroiwejr4eadfwefds"; // just an array of characters
-
-mylongString.length;
-
-// Object
-
-let minObject = {};
-
-let myCar = {
-  make: "Jeep",
-  color: "white",
-  year: "1998",
-  vin: "2874ihweoriy2380qoiu38r",
-};
-
-myCar.numDoors = 4;
-
-const anotherObject = {
-  wordz: ["foo", "bar", "baz"],
-  car: {
-    make: "McLaren",
-    model: "675LT",
-  },
-  awesomeness: true,
-};
-
-// Functions
-
-function myFunction() {
-  return "My greeting to you...";
-}
-
-function sumTwoThings(one, two) {
-  // watch out for data type issues here!
-  return one + two; // if numbers, will add them.  If strings, will concatenate.
-}
-
-// Arrow Functions
-
-(element) => console.log(element); // implicit 'return' when only one line for the function
-(element) => {
-  let foo = "bar" + "baxz";
-  return console.log(element); // explicit 'return' because of multiple lines
-};
-(num1, num2) => num1 + num2; /* Variables - containers that store values 
-   Multi-line comment here */
-
-var name; // a declared variable, but not initialized and it's in the global scope (BAD)
-
-let foo; // a declared variable that can be changed
-
-//const bar; // a declared variable that cannot be changed - short for 'constant'
-
-const ANSWER = 42; // const is declared and initialized with the value 42
-
-// Strings
-
-let string1 = "Hello World!"; // preferred way
-
-let string2 = new String("Hello World!"); // constructor
-
-// Number
-
-let myNum = 29038424;
-
-let myNum2 = 345.89;
-
-"1" == 1; // this statement is true because of type coercion and loose equality checking
-"1" === 1; // false because this is strict equality checking
-
-// Boolean
-
-let myBool = false;
-
-// Array
-
-let myArray = []; // this is an array
-
-//              0     1      2        3      4
-let myArray2 = [42, "Bob", myBool, ANSWER, true];
-
-let secondElement = myArray2[1]; // the second position is at index #1
-
-myArray2.push("Thor"); // added an element to the end of myArray2
-
-myArray2.unshift("Hello World!");
-
-let mylongString =
-  "32408usfjalieriweur938u425ksdjfowiur84uwrlwshdjfo8wuroiwejr4eadfwefds"; // just an array of characters
-
-mylongString.length;
-
-// Object
-
-let minObject = {};
-
-let myCar = {
-  make: "Jeep",
-  color: "white",
-  year: "1998",
-  vin: "2874ihweoriy2380qoiu38r",
-};
-
-myCar.numDoors = 4;
-
-const anotherObject = {
-  wordz: ["foo", "bar", "baz"],
-  car: {
-    make: "McLaren",
-    model: "675LT",
-  },
-  awesomeness: true,
-};
-
-// Functions
-
-function myFunction() {
-  return "My greeting to you...";
-}
-
-function sumTwoThings(one, two) {
-  // watch out for data type issues here!
-  return one + two; // if numbers, will add them.  If strings, will concatenate.
-}
-
-// Arrow Functions
-
-(element) => console.log(element); // implicit 'return' when only one line for the function
-(element) => {
-  let foo = "bar" + "baxz";
-  return console.log(element); // explicit 'return' because of multiple lines
-};
-(num1, num2) => num1 + num2; /* Variables - containers that store values 
-   Multi-line comment here */
-
-var name; // a declared variable, but not initialized and it's in the global scope (BAD)
-
-let foo; // a declared variable that can be changed
-
-//const bar; // a declared variable that cannot be changed - short for 'constant'
-
-const ANSWER = 42; // const is declared and initialized with the value 42
-
-// Strings
-
-let string1 = "Hello World!"; // preferred way
-
-let string2 = new String("Hello World!"); // constructor
-
-// Number
-
-let myNum = 29038424;
-
-let myNum2 = 345.89;
-
-"1" == 1; // this statement is true because of type coercion and loose equality checking
-"1" === 1; // false because this is strict equality checking
-
-// Boolean
-
-let myBool = false;
-
-// Array
-
-let myArray = []; // this is an array
-
-//              0     1      2        3      4
-let myArray2 = [42, "Bob", myBool, ANSWER, true];
-
-let secondElement = myArray2[1]; // the second position is at index #1
-
-myArray2.push("Thor"); // added an element to the end of myArray2
-
-myArray2.unshift("Hello World!");
-
-let mylongString =
-  "32408usfjalieriweur938u425ksdjfowiur84uwrlwshdjfo8wuroiwejr4eadfwefds"; // just an array of characters
-
-mylongString.length;
-
-// Object
-
-let minObject = {};
-
-let myCar = {
-  make: "Jeep",
-  color: "white",
-  year: "1998",
-  vin: "2874ihweoriy2380qoiu38r",
-};
-
-myCar.numDoors = 4;
-
-const anotherObject = {
-  wordz: ["foo", "bar", "baz"],
-  car: {
-    make: "McLaren",
-    model: "675LT",
-  },
-  awesomeness: true,
-};
-
-// Functions
-
-function myFunction() {
-  return "My greeting to you...";
-}
-
-function sumTwoThings(one, two) {
-  // watch out for data type issues here!
-  return one + two; // if numbers, will add them.  If strings, will concatenate.
-}
-
-// Arrow Functions
-
-(element) => console.log(element); // implicit 'return' when only one line for the function
-(element) => {
-  let foo = "bar" + "baxz";
-  return console.log(element); // explicit 'return' because of multiple lines
-};
-(num1, num2) => num1 + num2; /* Variables - containers that store values 
-   Multi-line comment here */
-
-var name; // a declared variable, but not initialized and it's in the global scope (BAD)
-
-let foo; // a declared variable that can be changed
-
-//const bar; // a declared variable that cannot be changed - short for 'constant'
-
-const ANSWER = 42; // const is declared and initialized with the value 42
-
-// Strings
-
-let string1 = "Hello World!"; // preferred way
-
-let string2 = new String("Hello World!"); // constructor
-
-// Number
-
-let myNum = 29038424;
-
-let myNum2 = 345.89;
-
-"1" == 1; // this statement is true because of type coercion and loose equality checking
-"1" === 1; // false because this is strict equality checking
-
-// Boolean
-
-let myBool = false;
-
-// Array
-
-let myArray = []; // this is an array
-
-//              0     1      2        3      4
-let myArray2 = [42, "Bob", myBool, ANSWER, true];
-
-let secondElement = myArray2[1]; // the second position is at index #1
-
-myArray2.push("Thor"); // added an element to the end of myArray2
-
-myArray2.unshift("Hello World!");
-
-let mylongString =
-  "32408usfjalieriweur938u425ksdjfowiur84uwrlwshdjfo8wuroiwejr4eadfwefds"; // just an array of characters
-
-mylongString.length;
-
-// Object
-
-let minObject = {};
-
-let myCar = {
-  make: "Jeep",
-  color: "white",
-  year: "1998",
-  vin: "2874ihweoriy2380qoiu38r",
-};
-
-myCar.numDoors = 4;
-
-const anotherObject = {
-  wordz: ["foo", "bar", "baz"],
-  car: {
-    make: "McLaren",
-    model: "675LT",
-  },
-  awesomeness: true,
-};
-
-// Functions
-
-function myFunction() {
-  return "My greeting to you...";
-}
-
-function sumTwoThings(one, two) {
-  // watch out for data type issues here!
-  return one + two; // if numbers, will add them.  If strings, will concatenate.
-}
-
-// Arrow Functions
-
-(element) => console.log(element); // implicit 'return' when only one line for the function
-(element) => {
-  let foo = "bar" + "baxz";
-  return console.log(element); // explicit 'return' because of multiple lines
-};
-(num1, num2) => num1 + num2; /* Variables - containers that store values 
-Multi-line comment here */
-
-var name; // a declared variable, but not initialized and it's in the global scope (BAD)
-
-let foo; // a declared variable that can be changed
-
-//const bar; // a declared variable that cannot be changed - short for 'constant'
-
-const ANSWER = 42; // const is declared and initialized with the value 42
-
-// Strings
-
-let string1 = "Hello World!"; // preferred way
-
-let string2 = new String("Hello World!"); // constructor
-
-// Number
-
-let myNum = 29038424;
-
-let myNum2 = 345.89;
-
-"1" == 1; // this statement is true because of type coercion and loose equality checking
-"1" === 1; // false because this is strict equality checking
-
-// Boolean
-
-let myBool = false;
-
-// Array
-
-let myArray = []; // this is an array
-
-//              0     1      2        3      4
-let myArray2 = [42, "Bob", myBool, ANSWER, true];
-
-let secondElement = myArray2[1]; // the second position is at index #1
-
-myArray2.push("Thor"); // added an element to the end of myArray2
-
-myArray2.unshift("Hello World!");
-
-let mylongString =
-  "32408usfjalieriweur938u425ksdjfowiur84uwrlwshdjfo8wuroiwejr4eadfwefds"; // just an array of characters
-
-mylongString.length;
-
-// Object
-
-let minObject = {};
-
-let myCar = {
-  make: "Jeep",
-  color: "white",
-  year: "1998",
-  vin: "2874ihweoriy2380qoiu38r",
-};
-
-myCar.numDoors = 4;
-
-const anotherObject = {
-  wordz: ["foo", "bar", "baz"],
-  car: {
-    make: "McLaren",
-    model: "675LT",
-  },
-  awesomeness: true,
-};
-
-// Functions
-
-function myFunction() {
-  return "My greeting to you...";
-}
-
-function sumTwoThings(one, two) {
-  // watch out for data type issues here!
-  return one + two; // if numbers, will add them.  If strings, will concatenate.
-}
-
-// Arrow Functions
-
-(element) => console.log(element); // implicit 'return' when only one line for the function
-(element) => {
-  let foo = "bar" + "baxz";
-  return console.log(element); // explicit 'return' because of multiple lines
-};
-(num1, num2) => num1 + num2;
+   var name; // a declared variable, but not initialized and it's in the global scope (BAD)
+
+   let foo; // a declared variable that can be changed
+   
+   //const bar; // a declared variable that cannot be changed - short for 'constant'
+   
+   const ANSWER = 42; // const is declared and initialized with the value 42
+   
+   // Strings
+   
+   let string1 = "Hello World!"  // preferred way
+   
+   let string2 = new String("Hello World!") // constructor
+   
+   // Number
+   
+   let myNum = 29038424;
+   
+   let myNum2 = 345.89
+   
+   "1" == 1; // this statement is true because of type coercion and loose equality checking
+   "1" === 1; // false because this is strict equality checking
+   
+   // Boolean
+   
+   let myBool = false;
+   
+   // Array
+   
+   let myArray = []; // this is an array
+   
+   //              0     1      2        3      4
+   let myArray2 = [42, "Bob", myBool, ANSWER, true];
+   
+   let secondElement = myArray2[1]; // the second position is at index #1
+   
+   myArray2.push("Thor"); // added an element to the end of myArray2
+   
+   myArray2.unshift("Hello World!");
+   
+   let mylongString =
+     "32408usfjalieriweur938u425ksdjfowiur84uwrlwshdjfo8wuroiwejr4eadfwefds"; // just an array of characters
+   
+   mylongString.length;
+   
+   // Object
+   
+   let minObject = {};
+   
+   let myCar = {
+     make: 'Jeep',
+     color: 'white',
+     year: '1998',
+     vin: '2874ihweoriy2380qoiu38r'
+   }
+   
+   myCar.numDoors = 4;
+   
+   const anotherObject = {
+     wordz: ["foo", "bar", "baz"],
+     car: {
+       make: "McLaren",
+       model: "675LT"
+     },
+     awesomeness: true
+   };
+   
+   // Functions
+   
+   function myFunction() {
+     return "My greeting to you...";
+   }
+   
+   function sumTwoThings(one, two) {
+     // watch out for data type issues here!
+     return one + two; // if numbers, will add them.  If strings, will concatenate.
+   }
+   
+   // Arrow Functions
+   
+   element => console.log(element) // implicit 'return' when only one line for the function
+   element => {
+     let foo = 'bar' + 'baxz'
+     return console.log(element) // explicit 'return' because of multiple lines
+   }
+   (num1, num2) => num1 + num2
+   
+   // basic syntax is num => 'The Num'
+   
+   // a higher order function is a function that accepts another function as a parameter.
+   // filter, map and reduce are the most popular, but forEach, every, find, and some are also HOFs
+   
+   const theFunction = () => {
+     //multiple lines use curly braces and 'return' keyword
+     return "I am awesome";
+   };
+   
+   // Filter method example.  Filter returns an array of all elements that 'pass the test'
+   const pilots = [
+     {
+       id: 2,
+       name: "Wedge Antilles",
+       faction: "Rebels"
+     },
+     {
+       id: 8,
+       name: "Ciena Ree",
+       faction: "Empire"
+     },
+     {
+       id: 40,
+       name: "Iden Versio",
+       faction: "Empire"
+     },
+     {
+       id: 66,
+       name: "Thane Kyrell",
+       faction: "Rebels"
+     }
+   ];
+   
+   const rebels = pilots.filter((pilot) => pilot.faction === "Rebels")
+   const empire = pilots.filter((pilot) => {
+     return pilot.faction === "Empire"
+   })
