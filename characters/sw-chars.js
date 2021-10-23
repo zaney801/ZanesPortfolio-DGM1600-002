5273c7b6758bfdba5e58da526fb93d81729dfee2
Zane Young <utahzane@gmail.com>
2021-10-22 21:43:05 -0600

    played around with css added comments for future

--- characters/sw-chars.js
+++ characters/sw-chars.js
@@ -1,40 +1,55 @@
-import { people } from '../data/people.js'
+import { people } from "../data/people.js";
 
-const main = document.querySelector('#main')
+const main = document.querySelector("#main");
 
-const maleCharacters = people.filter(person => person.gender === 'male')
-console.log(maleCharacters.length)
-const femaleCharacters = people.filter(person => person.gender === 'female')
-console.log(femaleCharacters.length)
+const maleCharacters = people.filter((person) => person.gender === "male");
+console.log(maleCharacters.length);
+const femaleCharacters = people.filter((person) => person.gender === "female");
+console.log(femaleCharacters.length);
 
-const otherCharacters = people.filter(person => {
-  if(person.gender === 'n/a' || person.gender === 'hermaphrodite') {
-    return person
+const otherCharacters = people.filter((person) => {
+  if (person.gender === "n/a" || person.gender === "hermaphrodite") {
+    return person;
   }
-})
+});
 
-console.log(otherCharacters)
+console.log(otherCharacters);
 
 femaleCharacters.forEach((element) => {
-  const personFig = document.createElement('figure')
-  const personImg = document.createElement('img')
-  let charNum = getLastNumber(element.url)
-  personImg.src = `https://starwars-visualguide.com/assets/img/characters/${charNum}.jpg`
-  const personCaption = document.createElement('figcaption')
-  personCaption.textContent = element.name
+  const personFig = document.createElement("figure");
+  const personImg = document.createElement("img");
+  let charNum = getLastNumber(element.url);
+  personImg.src = `https://starwars-visualguide.com/assets/img/characters/${charNum}.jpg`;
+  const personCaption = document.createElement("figcaption");
+  personCaption.textContent = element.name;
 
-  personFig.appendChild(personImg)
-  personFig.appendChild(personCaption)
+  personFig.appendChild(personImg);
+  personFig.appendChild(personCaption);
 
-  main.appendChild(personFig)
-  
-})
+  main.appendChild(personFig);
+});
 
+// male characters add tomorrow
+/*
+maleCharacters.forEach((element) => {
+  const personFig = document.createElement("figure");
+  const personImg = document.createElement("img");
+  let charNum = getLastNumber(element.url);
+  personImg.src = `https://starwars-visualguide.com/assets/img/characters/${charNum}.jpg`;
+  const personCaption = document.createElement("figcaption");
+  personCaption.textContent = element.name;
+
+  personFig.appendChild(personImg);
+  personFig.appendChild(personCaption);
+
+  main.appendChild(personFig);
+});
+*/
 function getLastNumber(url) {
-  let end = url.lastIndexOf('/')
-  let start = end - 2
-  if (url.charAt(start) === '/') {
-    start++
+  let end = url.lastIndexOf("/");
+  let start = end - 2;
+  if (url.charAt(start) === "/") {
+    start++;
   }
-  return url.slice(start, end)
+  return url.slice(start, end);
 }
