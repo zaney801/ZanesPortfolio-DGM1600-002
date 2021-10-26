74e6598a9a5d0a8eac6b33659bf4bec713728cac
Zane Young <utahzane@gmail.com>
2021-10-25 19:12:32 -0600

    other characters

--- characters/sw-chars.js
+++ characters/sw-chars.js
@@ -45,6 +45,22 @@ maleCharacters.forEach((element) => {
   main.appendChild(personFig);
 });
 
+// N/A characters
+
+otherCharacters.forEach((element) => {
+  const personFig = document.createElement("figure");
+  const personImg = document.createElement("img");
+  let charNum = getLastNumber(element.url);
+  personImg.src = `https://starwars-visualguide.com/assets/img/characters/${charNum}.jpg`;
+  const personCaption = document.createElement("figcaption");
+  personCaption.textContent = element.name;
+
+  personFig.appendChild(personImg);
+  personFig.appendChild(personCaption);
+
+  main.appendChild(personFig);
+});
+
 function getLastNumber(url) {
   let end = url.lastIndexOf("/");
   let start = end - 2;
