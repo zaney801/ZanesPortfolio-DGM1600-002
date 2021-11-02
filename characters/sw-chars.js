81c6b6d854c7b33f6e60d77868b873dc617c4731
Zane Young <utahzane@gmail.com>
2021-11-01 23:25:43 -0600

    formating and looking over code for errors.

--- characters/sw-chars.js
+++ characters/sw-chars.js
@@ -1,60 +1,60 @@
-import { people } from '../data/people.js'
-import { getLastNumber, removeChildren } from '../utils/index.js'
+import { people } from "../data/people.js";
+import { getLastNumber, removeChildren } from "../utils/index.js";
 
-const main = document.querySelector('#main')
+const main = document.querySelector("#main");
 
-const mainHeader = document.createElement('header')
-document.body.insertBefore(mainHeader, main)
+const mainHeader = document.createElement("header");
+document.body.insertBefore(mainHeader, main);
 
-const allButton = document.createElement('button')
-allButton.textContent = 'All Characters'
-allButton.addEventListener('click', () => populateDOM(people))
-mainHeader.appendChild(allButton)
+const allButton = document.createElement("button");
+allButton.textContent = "All Characters";
+allButton.addEventListener("click", () => populateDOM(people));
+mainHeader.appendChild(allButton);
 
-const maleButton = document.createElement('button')
-maleButton.textContent = 'Male Characters'
-maleButton.addEventListener('click', () => populateDOM(maleCharacters))
-mainHeader.appendChild(maleButton)
+const maleButton = document.createElement("button");
+maleButton.textContent = "Male Characters";
+maleButton.addEventListener("click", () => populateDOM(maleCharacters));
+mainHeader.appendChild(maleButton);
 
-const femaleButton = document.createElement('button')
-femaleButton.textContent = 'Female Characters'
-femaleButton.addEventListener('click', () => populateDOM(femaleCharacters))
-mainHeader.appendChild(femaleButton)
+const femaleButton = document.createElement("button");
+femaleButton.textContent = "Female Characters";
+femaleButton.addEventListener("click", () => populateDOM(femaleCharacters));
+mainHeader.appendChild(femaleButton);
 
-const othersButton = document.createElement('button')
-othersButton.textContent = 'Other Characters'
-othersButton.addEventListener('click', () => populateDOM(otherCharacters))
-mainHeader.appendChild(othersButton)
+const othersButton = document.createElement("button");
+othersButton.textContent = "Other Characters";
+othersButton.addEventListener("click", () => populateDOM(otherCharacters));
+mainHeader.appendChild(othersButton);
 
-const maleCharacters = people.filter((person) => person.gender === 'male')
+const maleCharacters = people.filter((person) => person.gender === "male");
 
-const femaleCharacters = people.filter((person) => person.gender === 'female')
+const femaleCharacters = people.filter((person) => person.gender === "female");
 
 const otherCharacters = people.filter((person) => {
   if (
-    person.gender === 'n/a' ||
-    person.gender === 'hermaphrodite' ||
-    person.gender === 'none'
+    person.gender === "n/a" ||
+    person.gender === "hermaphrodite" ||
+    person.gender === "none"
   ) {
-    return person
+    return person;
   }
-})
+});
 
 function populateDOM(characters) {
   // remove all the previous items before populating with new ones
-  removeChildren(main)
-  
+  removeChildren(main);
+
   characters.forEach((element) => {
-    const personFig = document.createElement('figure')
-    const personImg = document.createElement('img')
-    let charNum = getLastNumber(element.url)
-    personImg.src = `https://starwars-visualguide.com/assets/img/characters/${charNum}.jpg`
-    const personCaption = document.createElement('figcaption')
-    personCaption.textContent = element.name
+    const personFig = document.createElement("figure");
+    const personImg = document.createElement("img");
+    let charNum = getLastNumber(element.url);
+    personImg.src = `https://starwars-visualguide.com/assets/img/characters/${charNum}.jpg`;
+    const personCaption = document.createElement("figcaption");
+    personCaption.textContent = element.name;
 
-    personFig.appendChild(personImg)
-    personFig.appendChild(personCaption)
+    personFig.appendChild(personImg);
+    personFig.appendChild(personCaption);
 
-    main.appendChild(personFig)
-  })
+    main.appendChild(personFig);
+  });
 }
