3135290888838505cc16f87c8a81f4ce3ab56156
Zane Young <utahzane@gmail.com>
2021-11-02 23:15:30 -0600

    Star ship edit, added to main

--- starships/sw-starships.js
+++ starships/sw-starships.js
@@ -5,6 +5,18 @@ const nav = document.querySelector(".nav");
 const navList = document.querySelector(".navList");
 const shipView = document.querySelector(".shipViewer");
 
+const modal = document.querySelector(".modal");
+const closeButton = document.querySelector(".modal-close");
+const modalBackground = document.querySelector(".modal-background");
+const shipMessage = document.querySelector(".shipMessage");
+
+closeButton.addEventListener("click", () =>
+  modal.classList.toggle("is-active")
+);
+modalBackground.addEventListener("click", () =>
+  modal.classList.toggle("is-active")
+);
+
 function populateNav() {
   starships.forEach((starship) => {
     let anchorWrap = document.createElement("a");
@@ -23,9 +35,18 @@ populateNav();
 
 function populateShipView(shipData) {
   removeChildren(shipView);
-  console.log(`You clicked on ${shipData.name}`);
   let shipImage = document.createElement("img");
+  let shipName = document.createElement("div");
+  shipName.className = "shipName";
+  shipName.textContent = shipData.name;
   let shipNum = getLastNumber(shipData.url);
   shipImage.src = `https://starwars-visualguide.com/assets/img/starships/${shipNum}.jpg`;
+  shipImage.addEventListener("error", () => {
+    shipName.hidden = true;
+    shipImage.hidden = true;
+    modal.classList.toggle("is-active");
+    shipMessage.textContent = `The ship known as "${shipData.name}" has been canceled.`;
+  });
   shipView.appendChild(shipImage);
+  shipView.appendChild(shipName);
 }
