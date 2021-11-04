1162757e5efadf6f61f08c8bf5963cdc31614055
Zane Young <utahzane@gmail.com>
2021-11-04 13:03:41 -0600

    added congress

--- codejournal.js
+++ codejournal.js
@@ -126,3 +126,120 @@
    const empire = pilots.filter((pilot) => {
      return pilot.faction === "Empire"
    })
+   
+   // Array helper method 'map' example
+   
+   let filmURLs = [
+     "https://swapi.co/api/films/",
+     "https://swapi.co/api/films/5/",
+     "https://swapi.co/api/films/4/this one is longer... even longer",
+     "https://swapi.co/api/films/6/",
+     "https: ",
+     "https://swapi.co/api/films/1/"
+   ];
+   
+   const filmLengths = filmURLs.map((filmURL) => filmURL.length);
+   
+   const filmPlusMore = filmURLs.map((filmURL) => {
+     let filmObj = {
+       index: filmURL,
+       date: Date.now()
+     };
+     return filmObj;
+   });
+   
+   const pilotNames = pilots.map((pilot) => pilot.name);
+   
+   // Reduce example
+   
+   const swpilots = [
+     {
+       id: 10,
+       name: "Poe Dameron",
+       years: 14
+     },
+     {
+       id: 2,
+       name: "Temmin 'Snap' Wexley",
+       years: 30
+     },
+     {
+       id: 41,
+       name: "Tallissan Lintra",
+       years: 16
+     },
+     {
+       id: 99,
+       name: "Ello Asty",
+       years: 22
+     },
+   ];
+   
+   const totalYears = swpilots.reduce((acc, pilot) => acc + pilot.years, 0);
+   
+   const mostExpPilot = swpilots.reduce((oldest, pilot) => {
+     return (oldest.years || 0) > pilot.years ? oldest : pilot;
+   }, {});
+   
+   // Now we combine map, reduce, and filter
+   
+   const personnel = [
+     {
+       id: 5,
+       name: "Luke Skywalker",
+       pilotingScore: 98,
+       shootingScore: 56,
+       isForceUser: true
+     },
+     {
+       id: 82,
+       name: "Sabine Wren",
+       pilotingScore: 73,
+       shootingScore: 99,
+       isForceUser: false
+     },
+     {
+       id: 22,
+       name: "Zeb Orellios",
+       pilotingScore: 20,
+       shootingScore: 59,
+       isForceUser: false
+     },
+     {
+       id: 15,
+       name: "Ezra Bridger",
+       pilotingScore: 43,
+       shootingScore: 67,
+       isForceUser: true
+     },
+     {
+       id: 11,
+       name: "Caleb Dume",
+       pilotingScore: 71,
+       shootingScore: 85,
+       isForceUser: true
+     }
+   ];
+   
+   let jediPersonnel = personnel.filter((person) => person.isForceUser);
+   
+   let jediScores = jediPersonnel.map(
+     (jedi) => jedi.pilotingScore + jedi.shootingScore
+   );
+   
+   let totalJediScore = jediScores.reduce((acc, score) => acc + score, 0);
+   
+   const totalJediScoreChained = personnel
+     .filter((person) => person.isForceUser)
+     .map((jedi) => jedi.pilotingScore + jedi.shootingScore)
+     .reduce((acc, score) => acc + score, 0);
+   
+   // Ternary operator syntax: condition ? exprIfTrue : exprIfFalse
+   
+   const totalJediScoreReduce = personnel.reduce(
+     (acc, person) =>
+       person.isForceUser
+         ? acc + person.pilotingScore + person.shootingScore
+         : acc,
+     0
+   );
