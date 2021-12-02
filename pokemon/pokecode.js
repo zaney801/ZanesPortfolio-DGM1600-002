67ac4e4f10b73ca21f4cfa22ea4daf72903678db
Zane Young <utahzane@gmail.com>
2021-12-01 23:10:52 -0700

    formatting

--- pokemon/pokecode.js
+++ pokemon/pokecode.js
@@ -1,61 +1,64 @@
 function getAPIData(url) {
   try {
-    return fetch(url).then(data => data.json())
+    return fetch(url).then((data) => data.json());
   } catch (error) {
-    console.error(error)
+    console.error(error);
   }
 }
 
-getAPIData(`https://pokeapi.co/api/v2/pokemon?limit=25`)
-.then( async (data) => {
+getAPIData(`https://pokeapi.co/api/v2/pokemon?limit=25`).then(async (data) => {
   for (const pokemon of data.results) {
-    await getAPIData(pokemon.url).then(pokeData => populatePokeCards(pokeData))
+    await getAPIData(pokemon.url).then((pokeData) =>
+      populatePokeCards(pokeData)
+    );
   }
-})
+});
 
-const pokeGrid = document.querySelector('.pokeGrid')
+const pokeGrid = document.querySelector(".pokeGrid");
 
 function populatePokeCards(singlePokemon) {
-  const pokeScene = document.createElement('div')
-  pokeScene.className = 'scene'
-  const pokeCard = document.createElement('div')
-  pokeCard.className = 'card'
-  pokeCard.addEventListener('click', () => pokeCard.classList.toggle('is-flipped'))
-  
-  const front = populateCardFront(singlePokemon)
-  const back = populateCardBack(singlePokemon)
+  const pokeScene = document.createElement("div");
+  pokeScene.className = "scene";
+  const pokeCard = document.createElement("div");
+  pokeCard.className = "card";
+  pokeCard.addEventListener("click", () =>
+    pokeCard.classList.toggle("is-flipped")
+  );
 
-  pokeCard.appendChild(front)
-  pokeCard.appendChild(back)
-  pokeScene.appendChild(pokeCard)
-  pokeGrid.appendChild(pokeScene)
+  const front = populateCardFront(singlePokemon);
+  const back = populateCardBack(singlePokemon);
+
+  pokeCard.appendChild(front);
+  pokeCard.appendChild(back);
+  pokeScene.appendChild(pokeCard);
+  pokeGrid.appendChild(pokeScene);
 }
 
 function populateCardFront(pokemon) {
-  const pokeFront = document.createElement('figure')
-  pokeFront.className = 'cardFace front'
-  const pokeImg = document.createElement('img')
-  pokeImg.src = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${pokemon.id}.png`
+  const pokeFront = document.createElement("figure");
+  pokeFront.className = "cardFace front";
+  const pokeImg = document.createElement("img");
+  pokeImg.src = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${pokemon.id}.png`;
 
-  const pokeCaption = document.createElement('figcaption')
-  pokeCaption.textContent = pokemon.name
-  pokeFront.appendChild(pokeImg)
-  pokeFront.appendChild(pokeCaption)
-  return pokeFront
+  const pokeCaption = document.createElement("figcaption");
+  pokeCaption.textContent = pokemon.name;
+  pokeFront.appendChild(pokeImg);
+  pokeFront.appendChild(pokeCaption);
+  return pokeFront;
 }
 
 function populateCardBack(pokemon) {
-  const pokeBack = document.createElement('div')
-  pokeBack.className = 'cardFace back'
-  const label = document.createElement('h4')
-  label.textContent = 'Abilities:'
-  pokeBack.appendChild(label)
-  const abilityList = document.createElement('ul')
+  const pokeBack = document.createElement("div");
+  pokeBack.className = "cardFace back";
+  const label = document.createElement("h4");
+  label.textContent = "Abilities:";
+  pokeBack.appendChild(label);
+  const abilityList = document.createElement("ul");
   pokemon.abilities.forEach((abilityItem) => {
-    let listItem = document.createElement('li')
-    listItem.textContent = abilityItem.ability.name
-    abilityList.appendChild(listItem)
-  })
-  pokeBack.appendChild(abilityList)
-  return pokeBack
+    let listItem = document.createElement("li");
+    listItem.textContent = abilityItem.ability.name;
+    abilityList.appendChild(listItem);
+  });
+  pokeBack.appendChild(abilityList);
+  return pokeBack;
 }
